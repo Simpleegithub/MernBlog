6f9421854c5ae8c6226336f5a732e15ab7a13721
Muhammad Jawad <J45499738@gmail.comu@example.com>
2024-09-10 14:55:52 +0500

    first commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,7 +8,7 @@ import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 // import UpdatePost from "./pages/UpdatePost"
-import PrivateRoute from "./components/privateRoute";
+import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/updatePost";
